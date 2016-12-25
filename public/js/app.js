a721e223c15e39a3dcef513099f70c4ee2a76181
George Karaszi <georgekaraszi@gmail.com>
2016-12-25 16:20:17 -0500

    Added chatrooms

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,15 @@ var room = getQueryVariable("room");
 
 console.log(name + "wants to join " + room);
 
+jQuery('.room-title').append(room)
+
+
 socket.on('connect', function() {
     console.log('Connect to socket.io server');
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
+    })
 });
 
 socket.on('message', function(message){
